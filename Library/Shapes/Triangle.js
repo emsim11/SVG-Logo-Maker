fc7c78186e2cf2cc11009042a8d18b51f567646f
Emily Simone <emsimone11@gmail.com>
2024-04-23 14:17:42 -0400

    Update Triangle Dimensions

--- Library/Shapes/Triangle.js
+++ Library/Shapes/Triangle.js
@@ -6,7 +6,7 @@ class Triangle extends Shape {
     }
 
     render() {
-        return `<svg version='1.1' width='500' height='500' xmlns='http://www.w3.org/2000/svg'>\n  <polygon points='200, 10 10, 350 420, 350' fill='${this.ShapeColor}' stroke='${this.BorderColor}' stroke-width='${this.BorderWidth}'/>\n  <text x='40%' y='40%' font-size='80px' font-weight='700' text-anchor='middle' dominant-baseline='middle' fill='${this.TextColor}'>${this.Text}</text>\n</svg>`;
+        return `<svg version='1.1' width='500' height='500' xmlns='http://www.w3.org/2000/svg'>\n  <polygon points='200, 10 10, 350 420, 350' fill='${this.ShapeColor}' stroke='${this.BorderColor}' stroke-width='${this.BorderWidth}'/>\n  <text x='40%' y='45%' font-size='80px' font-weight='700' text-anchor='middle' dominant-baseline='middle' fill='${this.TextColor}'>${this.Text}</text>\n</svg>`;
     }
 }
 
